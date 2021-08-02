a26ad481e75707bb6d9d3414b665d09341b47143
William E Hahn <williamedwardhahn@gmail.com>
2021-08-01 22:52:21 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='DLP',
+   version='1.0',
+   description='Deep Learning Prep Tools',
+   author='William Edward Hahn',
+   author_email='williamedwardhahn@gmail.com',
+   packages=['DLP'],
+   install_requires=[],
+)
