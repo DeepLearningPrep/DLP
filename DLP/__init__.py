9af6be0ab9c8d6d8d72838330d96e0dc931fa12d
William E Hahn <williamedwardhahn@gmail.com>
2021-08-01 23:15:40 -0400

    Update __init__.py

--- DLP/__init__.py
+++ DLP/__init__.py
@@ -1,30 +1,25 @@
-import matplotlib.pyplot as plt
+import torch
 import imageio
-import torch
+import requests
 import torchvision
-from torchvision import models, transforms
-import numpy as np
-from torchvision.models import *
-from PIL import Image
-import requests
-from torchvision import models
-from torchsummary import summary
-import torch
-from skimage.util import montage
-from torch.autograd import Variable
-from torchvision import models, transforms
-from skimage import io as skio
-from scipy import signal
-import requests
 import numpy as np
 from math import exp
+from PIL import Image
 from time import time
+from scipy import signal
 from random import random
 from matplotlib import cm
 from bisect import bisect
 from random import uniform
 from random import randrange
+from torchvision import models
+from skimage import io as skio
 import matplotlib.pyplot as plt
+from torchvision.models import *
+from torchsummary import summary
+from skimage.util import montage
+from torch.autograd import Variable
 from scipy.special import factorial
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.colors import LightSource
+from torchvision import models, transforms
